9df47fa3fb87ad3799bd5531589eeeb307290bcc
AlbertSanfle <sanflealbert@gmail.com>
2024-02-29 20:15:11 -0400

    primer commint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'CalculadoraProyect';
+  title = 'Calculadora Proyect';
+  Number1: number = 0;
+  Number2: number = 0;
+  Resultado: number = 0;
+  sumar() {
+    this.Resultado = this.Number1 + this.Number2;
+  }
+  resta() {
+    this.Resultado = this.Number1 - this.Number2;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  Number1: number = 0;
+  Number2: number = 0;
+  Resultado: number = 0;
+  sumar() {
+    this.Resultado = this.Number1 + this.Number2;
+  }
+  Restar() {
+    this.Resultado = this.Number1 - this.Number2;
+  }
+}
